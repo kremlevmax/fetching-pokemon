0817b51d462dbc51200aeb4f84351e86cef6ae73
kremlevmax <kremlevmax1989@gmail.com>
2021-07-08 20:04:56 -0700

    Have error handling

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import { useState } from "react";
 function App() {
   const [pokemons, setPokemons] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchPokemons() {
-    setLoading(true);
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
-    const data = await response.json();
+    try {
+      setLoading(true);
+      const response = await fetch("https://pokeapi.co/api/v2/pokemons/");
 
-    data.results.map((pokemon) => {
-      setPokemons((oldArray) => [...oldArray, { name: pokemon.name }]);
-    });
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
-    setLoading(false);
+      const data = await response.json();
+
+      data.results.map((pokemon) => {
+        setPokemons((oldArray) => [...oldArray, { name: pokemon.name }]);
+      });
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
   }
 
   return (
@@ -22,7 +32,14 @@ function App() {
       <button className='pokemon-div' onClick={fetchPokemons}>
         Get Pokemons
       </button>
+
       {loading && "Loading Data"}
+      {!loading && error && error}
+
+      {!loading &&
+        !error &&
+        pokemons.length === 0 &&
+        "No Data, Sorry something went wrong"}
       {pokemons.map((pokemon) => (
         <div key={pokemon.name} className='pokemon-div'>
           {pokemon.name}
